03be3d84dd7a11e24a0939df5151b2902da2ae10
Miko Brandini <MikoBrandini@gmail.com>
2017-10-12 14:29:47 -0400

    Updated smoke test for ProductRow test file.

--- src/__tests__/ProductRow.test.js
+++ src/__tests__/ProductRow.test.js
@@ -2,7 +2,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ProductRow from '../components/ProductRow';
+product =
 
-it('renders without crashing', () => {
+it('renders ProductRow without crashing without crashing', () => {
   shallow(<ProductRow product={product} key={product.name} />);
 });
+
+//this object below exists in order to pass the smoke test above.
+var product = { name: "Peter Pan The Dark Lord"}
